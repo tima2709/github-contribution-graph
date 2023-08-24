7728a3abffe5558d488d1ff4d01fdf0226f811ed
tmrln2709 <tmrln2709@gmail.com>
2023-08-24 23:00:50 +0600

    done

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import {format} from "date-fns";
+
 
 
 const Timeline = ({cells}) => {
 
+    const week = ['mon', 'tue' , 'wed', 'thu', 'fri', 'sun', 'sut']
 
 
     return (
-        <div className={'timeline'}>
-            {
-                cells.map((el, index) => (
-                    <div key={index} className={'timeline-cell'} data-timeline-cell={el.date} data-commit={el.commit} style={{backgroundColor: `${el.color}`}}>
+        <div style={{display: 'flex', padding:'100px 20px', alignItems: 'center'}}>
+            <div style={{lineHeight: '17px'}}>
+                {week.map(el => (
+                    <p>{el}</p>
+                ))}
+            </div>
+            <div className={'timeline'}>
 
-                    </div>
-                ))
-            }
+                {
+                    cells.map((el, index) => (
+                        <div key={index} className={'timeline-cell'} data-timeline-cell={el.date} data-commit={el.commit} style={{backgroundColor: `${el.color}`}}>
+
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 };
